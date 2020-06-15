d2fa3da52d292be7b5edce83568ac53c939bbdf7
Tiffany Wang (TIFW) <63823036+tifw-cg@users.noreply.github.com>
2020-06-15 15:30:43 -0700

    Added simple exercises
    10 samples

---
+++ Add two numbers.py
@@ -0,0 +1,9 @@
+#reference: https://www.programiz.com/python-programming/examples/add-number
+
+num1 = 1.5
+num2 = 6.3
+
+# Add two numbers
+sum = num1 + num2
+
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
